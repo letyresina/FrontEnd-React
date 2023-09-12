dc088e41626f98e3383ca2a1ee8e1ab856014c6e
letyresina <leticiaresina@gmail.com>
2023-09-12 11:47:29 -0300

    Subindo arquivo

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <section className='estrutura'>
+          <article className='box'>
+            <h1>Nossa estrutura</h1>
+            <div className='imagem'>
+              <img src="https://picsum.photos/350/200" alt="Imagem aleatória"></img>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi in nisi sunt ullam similique id 
+                veritatis fuga quo doloremque architecto hic cumque doloribus inventore nesciunt, recusandae eaque 
+                tempora reiciendis distinctio.
+              </p>
+            </div>
+            <a href="#">
+              Veja Mais
+            </a>
+          </article>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
