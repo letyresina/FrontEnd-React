31358ac768bfc435688093fb1ab65e5dc76831ac
letyresina <leticiaresina@gmail.com>
2023-09-19 11:16:10 -0300

    entendendo responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,44 @@ function App() {
                 tempora reiciendis distinctio.
               </p>
             </div>
+            <div className='imagem'>
+              <img src="https://picsum.photos/350/200" alt="Imagem aleatória"></img>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi in nisi sunt ullam similique id 
+                veritatis fuga quo doloremque architecto hic cumque doloribus inventore nesciunt, recusandae eaque 
+                tempora reiciendis distinctio.
+              </p>
+            </div>
             <a href="#">
               Veja Mais
             </a>
           </article>
+        </section>
+
+        <section className='estrutura-card'>
+
+          <h1>Criando Mixins</h1>
+          <div>
+            <article>
+              <p>
+                Lorem
+              </p>
+            </article>
+
+            <article>
+              <p>
+                Lorem
+              </p>
+            </article>
+
+            
+            <article>
+              <p>
+                Lorem
+              </p>
+            </article>
+          </div>
+
         </section>
       </div>
     </>
